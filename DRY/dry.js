c66777d8b6430822d9effa567a421436238d6d24
Aaronleon13 <aaronleon13@gmail.com>
2026-03-08 18:29:36 -0700

    feat: Add initial functions for user registration, update, and deletion with duplicated validation logic.

---
+++ DRY/dry.js
@@ -0,0 +1,32 @@
+// En un sistema necesitamos validar información de usuarios antes de guardarla.
+// Actualmente el sistema tiene este código:
+
+function registrarUsuario(usuario) {
+  if (usuario.nombre === "") {
+    console.log("El nombre es obligatorio");
+  }
+
+  if (usuario.email === "") {
+    console.log("El email es obligatorio");
+  }
+}
+
+function actualizarUsuario(usuario) {
+  if (usuario.nombre === "") {
+    console.log("El nombre es obligatorio");
+  }
+
+  if (usuario.email === "") {
+    console.log("El email es obligatorio");
+  }
+}
+
+function eliminarUsuario(usuario) {
+  if (usuario.nombre === "") {
+    console.log("El nombre es obligatorio");
+  }
+
+  if (usuario.email === "") {
+    console.log("El email es obligatorio");
+  }
+}
