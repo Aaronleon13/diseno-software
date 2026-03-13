a5789e13ed513e3d6c8a5e865e021694f78382cd
Aaronleon13 <aaronleon13@gmail.com>
2026-03-13 09:37:25 -0700

    refactor: switch from inheritance to composition for `Pajaro` and `Avion` classes.

--- CSH/index.js
+++ CSH/index.js
@@ -3,17 +3,76 @@
 //un avión no es un pájaro
 //solo comparten comportamiento
 
-class Volador {
+// class Volador {
+//   volar() {
+//     console.log("Está volando");
+//   }
+// }
+
+// class Pajaro extends Volador {}
+// class Avion extends Volador {}
+
+// const pajaro = new Pajaro();
+// pajaro.volar();
+
+// const avion = new Avion();
+// avion.volar();
+
+//Composicion
+
+class Volar {
   volar() {
     console.log("Está volando");
   }
 }
 
-class Pajaro extends Volador {}
-class Avion extends Volador {}
+class Nadar {
+  nadar() {
+    console.log("Está nadando");
+  }
+}
 
-const pajaro = new Pajaro();
-pajaro.volar();
+class Pajaro {
+  constructor() {
+    this.volador = new Volar();
+    this.nadador = new Nadar();
+  }
 
-const avion = new Avion();
-avion.volar();
+  volar() {
+    this.volador.volar();
+  }
+
+  nadar() {
+    this.nadador.nadar();
+  }
+}
+
+class Avion {
+  constructor() {
+    this.volador = new Volar();
+  }
+
+  volar() {
+    this.volador.volar();
+  }
+}
+console.log("-----------Pajaros-----------");
+const paloma = new Pajaro();
+paloma.volar();
+
+const halcon = new Pajaro();
+halcon.volar();
+
+const pato = new Pajaro();
+pato.nadar();
+pato.volar();
+
+console.log("-----------Aviones-----------");
+const boing747 = new Avion();
+boing747.volar();
+
+const jet = new Avion();
+jet.volar();
+
+const cessna402 = new Avion();
+cessna402.volar();
